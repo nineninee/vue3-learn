271b138fc7f5c9e56ac4bdb110a81eab11779278
nineninee <1264178545@qq.com>
2024-02-20 18:04:13 +0800

    路由

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
 <template>
-  <Person />
+  <div class="app">
+    <h2>路由</h2>
+    <div class="navigate">
+      <RouterLink  to="/home" active-class="active">首頁</RouterLink>
+      <RouterLink  to="/news"   active-class="active">新聞</RouterLink>
+      <RouterLink  to="/about"  active-class="active">關於</RouterLink>
+    </div>
+    <div class="main-content">
+      <RouterView></RouterView>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import Person from '@/components/Person.vue'
+import { RouterView, RouterLink } from 'vue-router';
 </script>
-
 <style>
+.navigate {
+  margin: 20px;
+  height: 50px;
+  background: pink;
+  display: flex;
+  justify-content: space-around;
+  a {
+    line-height: 50px;
+    text-decoration: none;
+    color: blueviolet;
+  }
+  a.active {
+    color: yellow;
+  }
+}
+
+.main-content {
+  margin: 20px;
+  height: 200px;
+  background: pink;
+}
 </style>
