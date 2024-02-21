239d2d10c97f735ec6bf46b71533d7efb801a734
nineninee <1264178545@qq.com>
2024-02-21 16:30:25 +0800

    路由嵌套

---
+++ 备份/20240221路由嵌套/src/components/Detail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>id: xxx</p>
+    <p>title: xxx</p>
+    <p>content: xxx</p>
+  </div>
+</template>
+<script setup lang='ts'>
+
+</script>
+
+<style scoped lang='scss'>
+
+</style>
---
+++ 备份/20240221路由嵌套/src/routers/index.ts
@@ -0,0 +1,41 @@
+// 创建一个路由器，并暴露出去
+
+// 第一步：引入createRouter
+import { createRouter, createWebHistory } from "vue-router";
+// 引入相关组件
+import Home from "../components/Home.vue";
+import About from "../components/About.vue";
+import News from "../components/News.vue";
+import Detail from "../components/Detail.vue";
+
+// 第二步：创建路由器
+const router = createRouter({
+  history: createWebHistory(), //路由器的工作模式
+  routes: [
+    {
+      name: "zhuye",
+      path: "/home",
+      component: Home,
+    },
+    {
+      name: "xinwen",
+      path: "/news",
+      component: News,
+      children: [
+        {
+          path: "detail",
+          component: Detail,
+        },
+      ],
+    },
+    {
+      name: "guanyu",
+      path: "/about",
+      component: About,
+    },
+  ],
+});
+
+export default router;
+
+// 暴露router
