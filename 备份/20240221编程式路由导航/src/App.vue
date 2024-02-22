f08e461abfafbc96b9fd034553dbcadcf4557bc4
nineninee <1264178545@qq.com>
2024-02-22 15:00:58 +0800

    编程式路由导航

---
+++ 备份/20240221编程式路由导航/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app">
+    <h2>编程式路由导航</h2>
+    <h2>RouterLink的底层实现原理是a标签，当我们需要不借助a标签和不点击跳转的时候需要用到编程式路由导航</h2>
+    <div class="navigate">
+      <RouterLink replace to="/home" active-class="active">首頁</RouterLink>
+
+      <!-- <RouterLink  to="/news"   active-class="active">新聞</RouterLink> -->
+      <RouterLink replace :to="{name: 'xinwen'}"   active-class="active">新聞</RouterLink>
+
+      <!-- <RouterLink  to="/about"  active-class="active">關於</RouterLink> -->
+      <RouterLink  :to="{path: '/about'}"  active-class="active">關於</RouterLink>
+    </div>
+    <div class="main-content">
+      <RouterView></RouterView>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { RouterView, RouterLink } from 'vue-router';
+</script>
+<style>
+.navigate {
+  margin: 20px;
+  height: 50px;
+  background: pink;
+  display: flex;
+  justify-content: space-around;
+  a {
+    line-height: 50px;
+    text-decoration: none;
+    color: blueviolet;
+  }
+  a.active {
+    color: yellow;
+  }
+}
+
+.main-content {
+  margin: 20px;
+  height: 200px;
+  background: pink;
+}
+</style>
---
+++ 备份/20240221编程式路由导航/src/components/News.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="news">
+    <!-- 导航区 -->
+    <ul>
+      <li v-for="news in newsList">
+        <button @click="getDetail(news)">查看详情</button>
+        <RouterLink
+          :to="{
+            name: `xiangqing`,
+            query: { id: news.id, title: news.title, content: news.content },
+          }"
+          >
+          {{ news.title }}
+        </RouterLink>
+      </li>
+    </ul>
+    <!-- 内容区 -->
+    <div>
+      <RouterView></RouterView>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+interface NewsInter{
+  id: string,
+  title: string,
+  content: string
+}
+
+const newsList = reactive([
+  { id: "dddasdfa1", title: "news1", content: "news1 content" },
+  { id: "dddasdfa2", title: "news2", content: "news2 content" },
+  { id: "dddasdfa3", title: "news3", content: "news3 content" },
+]);
+
+
+function getDetail(news: NewsInter) {
+  // router.push({
+  //   name: `xiangqing`,
+  //   query: { id: news.id, title: news.title, content: news.content },
+  // })
+
+  router.replace({
+    name: `xiangqing`,
+    query: { id: news.id, title: news.title, content: news.content },
+  })
+}
+</script>
+
+<style scoped>
+.news {
+  display: flex;
+  gap: 50px;
+}
+</style>
