9ad4d39ab809767bbf1a3ec7f0b30761694a3073
nineninee <1264178545@qq.com>
2024-02-22 15:46:53 +0800

    pinia的存、读、改

---
+++ 备份/20240222pinia的存、读、改/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <h2>pinia</h2>
+    <Count></Count>
+    <hr>
+    <Love></Love>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Count from '@/components/Count.vue'
+import Love from '@/components/Love.vue'
+
+</script>
+<style>
+</style>
---
+++ 备份/20240222pinia的存、读、改/src/components/Count.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ countStore.sum }}</h1>
+    <button @click="add">加</button>
+  </div>
+</template>
+<script setup lang='ts'>
+import { ref } from 'vue'
+import { useCountStore } from '@/store/count'
+const countStore = useCountStore()
+
+function add() {
+  // 第一种修改方式
+  // countStore.sum++
+
+  // 第二种修改方式
+  // countStore.$patch({
+  //   sum: countStore.sum++,
+  //   student: '张三'
+  // })
+
+  // 第三种修改方式 action
+  countStore.increatments()
+}
+
+</script>
+
+<style scoped lang='scss'>
+
+</style>
