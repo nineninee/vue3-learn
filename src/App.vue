f2438edf9fb94d5e1ef8d9f61efea7ad5ff4d2a3
nineninee <1264178545@qq.com>
2024-02-23 16:47:18 +0800

    作用域插槽

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="app">
     <Category>
-      <template v-slot:content>
+      <!-- 获取slot传过来的所有参数 -->
+      <!-- <template v-slot:content="data"> -->
+
+      <!-- 解构slot传过来的参数 -->
+      <template v-slot:content="{ content: personList }">
+        <!-- <template v-slot:content="data"> -->
         <ul>
+          <!-- 获取slot传过来的所有参数 -->
+          <!-- <li v-for="item in data.content">{{ item.name }}</li> -->
+
+          <!-- 解构slot传过来的参数 -->
           <li v-for="item in personList">{{ item.name }}</li>
+
         </ul>
       </template>
       <template v-slot:title>
@@ -11,37 +21,25 @@
       </template>
     </Category>
 
-    <Category title="animal">
-      <template #content>
-        <ul>
-          <li v-for="item in animalList">{{ item.name }}</li>
-        </ul>
+    <Category>
+      <template #content="{ content: personList }">
+      <!-- <template v-slot:content="{ content: personList }"> -->
+        <h3 v-for="item in personList">{{ item.name }}</h3>
       </template>
       <template v-slot:title>
-        <h2>Animal</h2>
+        <h2>Person</h2>
       </template>
     </Category>
+
   </div>
 </template>
 
 <script lang="ts" setup>
 import Category from "./components/Category.vue";
-import { provide, ref } from "vue";
 
-const personList = ref([
-  { id: "001", name: "张三" },
-  { id: "002", name: "李四" },
-  { id: "003", name: "王五" },
-]);
-const animalList = ref([
-  { id: "001", name: "ji" },
-  { id: "002", name: "ya" },
-  { id: "003", name: "gou" },
-]);
+// 作用域插槽
+// 父组件提供结构，子组件提供数据
 
-
-// v-slot只能用在组件和template标签上
-// v-slot:slotname 可以简写为  #slotname
 </script>
 <style>
 .app {
--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,13 +1,21 @@
 <template>
 <div class="category">
   <slot name="title">默认标题</slot>
-  <slot name="content">默认内容</slot>
-
+  <slot name="content" :content="personList" x="haha" y="hehe">默认内容</slot>
 </div>
 </template>
 <script setup lang='ts'>
+import { provide, ref } from "vue";
 
 defineProps(['title'])
+
+
+
+const personList = ref([
+  { id: "001", name: "张三" },
+  { id: "002", name: "李四" },
+  { id: "003", name: "王五" },
+]);
 
 </script>
 
