6226482ccf35b2939bb6f671240d0acfbac794b0
nineninee <1264178545@qq.com>
2024-02-23 16:13:21 +0800

    默认插槽

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div class="app">
-    <h2>pinia</h2>
-    <Count></Count>
-    <hr>
-    <Love></Love>
+    <Category title="person">
+      <ul>
+        <li v-for="item in personList">{{ item.name }}</li>
+      </ul>
+    </Category>
+    <Category title="animal">
+      <ul>
+        <li v-for="item in animalList">{{ item.name }}</li>
+      </ul>
+    </Category>
   </div>
 </template>
 
 <script lang="ts" setup>
-import Count from '@/components/Count.vue'
-import Love from '@/components/Love.vue'
+import Category from './components/Category.vue';
+import { provide, ref } from 'vue';
+
+const personList = ref([{id: '001',name:'张三'},{id:'002',name:'李四'},{id:'003',name:'王五'}]);
+const animalList = ref([{id: '001',name:'ji'},{id:'002',name:'ya'},{id:'003',name:'gou'}]);
+
+
 
 </script>
 <style>
+.app{
+  width: 500px;
+  height: 300px;
+  display: flex;
+  justify-content: space-evenly;
+  background-color: lightblue;
+}
 </style>
---
+++ src/components/Category.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="category">
+  <div>{{ title }}</div>
+  <slot></slot>
+</div>
+</template>
+<script setup lang='ts'>
+
+defineProps(['title'])
+
+</script>
+
+<style scoped>
+.category{
+  width: 100px;
+  height: 200px;
+  background-color: pink;
+}
+
+</style>
