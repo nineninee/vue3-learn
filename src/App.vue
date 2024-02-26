4bdbe761c43775327e2d56a7c9de964501221655
nineninee <1264178545@qq.com>
2024-02-26 18:30:35 +0800

    customRef

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,31 @@
 <template>
   <div class="app">
-    <Category>
-      <!-- 获取slot传过来的所有参数 -->
-      <!-- <template v-slot:content="data"> -->
-
-      <!-- 解构slot传过来的参数 -->
-      <template v-slot:content="{ content: personList }">
-        <!-- <template v-slot:content="data"> -->
-        <ul>
-          <!-- 获取slot传过来的所有参数 -->
-          <!-- <li v-for="item in data.content">{{ item.name }}</li> -->
-
-          <!-- 解构slot传过来的参数 -->
-          <li v-for="item in personList">{{ item.name }}</li>
-
-        </ul>
-      </template>
-      <template v-slot:title>
-        <h2>Person</h2>
-      </template>
-    </Category>
-
-    <Category>
-      <template #content="{ content: personList }">
-      <!-- <template v-slot:content="{ content: personList }"> -->
-        <h3 v-for="item in personList">{{ item.name }}</h3>
-      </template>
-      <template v-slot:title>
-        <h2>Person</h2>
-      </template>
-    </Category>
-
+    <h1>{{ msg }}</h1>
+    <input type="text" v-model="msg">
   </div>
 </template>
 
 <script lang="ts" setup>
-import Category from "./components/Category.vue";
+import { customRef } from 'vue'
 
-// 作用域插槽
-// 父组件提供结构，子组件提供数据
+let initValue = 'hello'
+let timer = null
+let msg = customRef((track, trigger) => {
+  return {
+    get() {
+      track()  // 告诉vue数据msg很重要，你要对msg进行持续关注，一旦msg变化就去更新
+      return initValue
+    },
+    set(newValue) {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        initValue = newValue
+        trigger() // 告诉vue一下msg变化了
+      }, 1000)
+    }
+  }
+})
 
 </script>
 <style>
-.app {
-  width: 500px;
-  height: 300px;
-  display: flex;
-  justify-content: space-evenly;
-  background-color: lightblue;
-}
-h2 {
-  text-align: center;
-  background-color: yellow;
-}
 </style>
